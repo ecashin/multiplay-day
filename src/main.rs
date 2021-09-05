b891189dfe44c00f3323c6c1ce5b2f5dd6ab4b27
Ed Cashin <ed.cashin@acm.org>
2021-09-05 12:06:12 -0400

    Add major components

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,55 @@
 use yew::prelude::*;
 
+const N_REQUIRED: usize = 3;
+const N_CHOICES: usize = 4;
+
 enum Msg {
-    AddOne,
+    ChoiceMade(i32),
 }
 
 struct Model {
-    // `ComponentLink` is like a reference to a component.
-    // It can be used to send messages to the component
     link: ComponentLink<Self>,
-    value: i64,
+    history: Vec<bool>,
+    problem: (i32, i32),
+    choices: Vec<i32>,
+}
+
+impl Model {
+    fn progress_bar(&self) -> Html {
+        let n_correct = self.history.iter().copied().filter(|p| *p).count();
+        let check = "✅";
+        let x_mar = "❌";
+        let history_viz: Vec<&str> = self
+            .history
+            .iter()
+            .copied()
+            .map(|correct| if correct { check } else { x_mar })
+            .collect();
+        html! {
+            <span>
+                <p>{ format!("{} / {} - {}", n_correct, N_REQUIRED, history_viz.join("")) }</p>
+            </span>
+        }
+    }
+    fn problem_display(&self) -> Html {
+        html! {
+            <p>{ format!("{} x {} = ...", self.problem.0, self.problem.1) }</p>
+        }
+    }
+    fn choices_display(&self) -> Html {
+        self.choices.iter().copied().map(|response| {
+            html! {
+                <div>
+                    <button onclick=self.link.callback(move |_| Msg::ChoiceMade(response))>{ response }</button>
+                </div>
+            }
+        })
+        .collect()
+    }
+}
+
+fn new_problem(_model: Option<&Model>) -> ((i32, i32), Vec<i32>) {
+    ((6, 4), vec![24, 2, 3, 4])
 }
 
 impl Component for Model {
@@ -16,32 +57,39 @@ impl Component for Model {
     type Properties = ();
 
     fn create(_props: Self::Properties, link: ComponentLink<Self>) -> Self {
-        Self { link, value: 0 }
+        let (problem, choices) = new_problem(None);
+        Self {
+            link,
+            history: vec![],
+            problem,
+            choices,
+        }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            Msg::ChoiceMade(response) => {
+                let answer = self.problem.0 * self.problem.1;
+                let correct = response == answer;
+                self.history.push(correct);
+                let (problem, choices) = new_problem(Some(self));
+                self.problem = problem;
+                self.choices = choices;
                 true
             }
         }
     }
 
     fn change(&mut self, _props: Self::Properties) -> ShouldRender {
-        // Should only return "true" if new properties are different to
-        // previously received properties.
-        // This component has no properties so we will always return "false".
         false
     }
 
     fn view(&self) -> Html {
         html! {
             <div>
-                <button onclick=self.link.callback(|_| Msg::AddOne)>{ "+1" }</button>
-                <p>{ self.value }</p>
+                <div>{ self.progress_bar() }</div>
+                <div>{ self.problem_display() }</div>
+                <div>{ self.choices_display() }</div>
             </div>
         }
     }
