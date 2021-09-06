d42bab678f884d90fc15a58f38ab1396e53a9fc6
Ed Cashin <ed.cashin@acm.org>
2021-09-06 11:20:15 -0400

    Add sounds on correct user responses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use js_sys::Date;
 use rand::prelude::*;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use web_sys::HtmlAudioElement;
 use yew::format::Json;
 use yew::prelude::*;
 use yew::services::storage::{Area, StorageService};
@@ -17,6 +18,16 @@ const N_CHOICES: usize = 4;
 const MAX_FACTOR: usize = 12;
 const SUFFICIENT: usize = 2;
 const FAST_MILLISECONDS: f64 = 2000.0;
+const SOUND_FILES: &'static [&'static str] = &[
+    "broccoli1.wav",
+    "carrot1.wav",
+    "carrot2.wav",
+    "corn.wav",
+    "potato.wav",
+    "squash1.wav",
+    "zuccini1.wav",
+    "zuccini2.wav",
+];
 // https://dev.to/davidedelpapa/yew-tutorial-04-and-services-for-all-1non
 const STORAGE_KEY: &'static str = "net.noserose.multiplay";
 
@@ -67,6 +78,7 @@ struct Model {
     feedback: Option<String>,
     storage: StorageService,
     tally: Tally,
+    sounds: Vec<HtmlAudioElement>,
 }
 
 fn create_pairs_matrix() -> Vec<Vec<PairStatus>> {
@@ -194,6 +206,28 @@ impl Model {
             </table>
         }
     }
+
+    fn play(&self) {
+        let mut rng = thread_rng();
+        let i = rng.gen_range(0..SOUND_FILES.len());
+        let msg = match self.sounds[i].play() {
+            Ok(result) => format!("play {} success: {:?}", SOUND_FILES[i], result),
+            Err(e) => format!("play {} error: {:?}", SOUND_FILES[i], e),
+        };
+        console_log(&msg);
+    }
+
+    fn audio_elements(&self) -> Html {
+        SOUND_FILES
+            .iter()
+            .copied()
+            .map(|f| {
+                html! {
+                    <audio src=f id=f preload="auto" crossorigin="anonymous"></audio>
+                }
+            })
+            .collect()
+    }
 }
 
 fn choose_choices(rng: &mut ThreadRng, model: Option<&Model>) -> Vec<Problem> {
@@ -257,6 +291,10 @@ impl Component for Model {
         let storage = StorageService::new(Area::Local).unwrap();
         let Json(tally) = storage.restore(STORAGE_KEY);
         let tally = tally.unwrap_or(Tally::new()).valid_or_new();
+        let sounds = SOUND_FILES
+            .iter()
+            .map(|f| HtmlAudioElement::new_with_src(f).expect("Creating HtmlAudioElement"))
+            .collect();
         Self {
             link,
             history: vec![],
@@ -267,6 +305,7 @@ impl Component for Model {
             feedback: None,
             storage,
             tally,
+            sounds,
         }
     }
 
@@ -276,6 +315,9 @@ impl Component for Model {
                 console_log(&format!("ChoiceMade:{}", response));
                 let answer = self.problem.0 * self.problem.1;
                 let correct = response == answer;
+                if correct {
+                    self.play();
+                }
                 self.history.push((self.problem, correct));
                 self.update_pairs(correct);
                 self.update_storage(correct);
@@ -320,6 +362,7 @@ impl Component for Model {
                 <div>{ self.problem_display() }</div>
                 <div class="flex demo">{ self.choices_display() }</div>
                 <div>{ self.matrix_display() }</div>
+                <div>{ self.audio_elements() }</div>
             </div>
         }
     }
